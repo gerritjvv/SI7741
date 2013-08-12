4f2574fc01d05ab5bb3bc134261b963ee89793eb
gerritjvv <gerritjvv@gmail.com>
2013-08-12 14:05:10 +0200

    [SI7741] adding compiled test jar

--- si7741/test.sh
+++ si7741/test.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-MVN_REPO="/home/gvanvuuren/checkouts/SI7741/si7741/test-lib"
+BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+LIB="$BASE/test-lib"
 
 
 
-CP="/home/gvanvuuren/checkouts/SI7741/si7741/target/si7741-0.1.0.jar"
+CP="$BASE/target/si7741-0.1.0.jar"
 
-for f in $MVN_REPO/*.jar; do
-CP="$CP:$f"
+for f in $LIB/*.jar; do
+ CP="$CP:$f"
 done
 
 BOOT=""
-for f in $MVN_REPO/scala*.jar; do
-BOOT="$BOOT:$f"
+for f in $LIB/scala*.jar; do
+ BOOT="$BOOT:$f"
 done
 
-echo $BOOT
 java -Xbootclasspath/a:"$BOOT" -classpath "$CP" test.si7741.UsingIMain $@
